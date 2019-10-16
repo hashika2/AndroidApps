ec43586764eeafb63a96b94cae0bd9ccd279d698
hashika <m.g.hashikamaduranga@gmail.com>
2019-10-16 22:37:46 +0530

    add input text fied and button

--- App/app/src/main/java/com/example/app/SecondActivity.kt
+++ App/app/src/main/java/com/example/app/SecondActivity.kt
@@ -1,5 +1,6 @@
 package com.example.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
@@ -9,5 +10,13 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+//        Register.setOnClickListener{
+//
+//            val intent = Intent(this,SecondActivity::class.java)
+//            startActivity(intent)
+//        }
     }
+
+
 }
