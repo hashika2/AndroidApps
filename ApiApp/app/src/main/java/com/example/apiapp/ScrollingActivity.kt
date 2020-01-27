3aaa62a37cd37c3f86ddc70d571d83f25851021c
hashika <m.g.hashikamaduranga@gmail.com>
2020-01-27 21:28:02 +0530

    create a app for show api get request in json placsholder

---
+++ ApiApp/app/src/main/java/com/example/apiapp/ScrollingActivity.kt
@@ -0,0 +1,57 @@
+package com.example.apiapp
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
+import kotlinx.android.synthetic.main.activity_scrolling.*
+
+class ScrollingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrolling)
+        setSupportActionBar(toolbar)
+
+
+        fab.setOnClickListener {
+            val httpAsync = "https://httpbin.org/get"
+                .httpGet()
+                .responseString { request, response, result ->
+//                    println(request)
+//                    println(response)
+                    when (result) {
+                        is Result.Failure -> {
+                            val ex = result.getException()
+                            println(ex)
+                        }
+                        is Result.Success -> {
+                            val data = result.get()
+                            println(data)
+                        }
+                    }
+                }
+
+            httpAsync.join()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_scrolling, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
